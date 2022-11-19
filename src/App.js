2d64309b37ba10ba6262b1d69c540ab325a76eca
seoyeonko <seoyeon99o@naver.com>
2022-11-20 01:55:12 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import MyHeader from './MyHader';
+import MyFooter from './MyFooter';
 
 function App() {
+  const style = {
+    App: {
+      backgroundColor: 'black',
+      color: 'white',
+    },
+    h2: {
+      color: 'red',
+    },
+    bold_text: {
+      color: 'green',
+    },
+  };
+
+  const func = () => {
+    return 'func';
+  };
+
+  const number = 5;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={style.App} className="App">
+      <MyHeader />
+      <h2 style={style.h2}>안뇽 {func()}</h2>
+      <b style={style.bold_text} id="bold_text">
+        React.js
+      </b>
+      <b style={{ backgroundColor: 'hotpink' }} id="bold_text">
+        {number}는 {number % 2 === 0 ? '짝수' : '홀수'}
+      </b>
+
+      <MyFooter />
     </div>
   );
 }

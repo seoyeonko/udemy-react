3489a69541713448aeeb971e51621051bcccada5
seoyeonko <seoyeon99o@naver.com>
2022-11-20 15:22:35 +0900

    #34. 5-5. Props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import './App.css';
 // import MyHeader from './MyHader';
 // import MyFooter from './MyFooter';
+import Container from './Container';
 import Counter from './Counter';
 
 function App() {
@@ -36,10 +37,23 @@ function App() {
   // );
 
   // #33. State
+  // #34. Props
+  // : App 컴포넌트에서 Counter 컴포넌트에게 전해준 값으로 카운터 기능을 수행해야 하는 경우
+  const counterProps = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    initialValue: 5,
+  };
   return (
-    <>
-      <Counter />
-    </>
+    <Container>
+      <div>
+        {/* <Counter a={'hello'} initialValue={5} /> */}
+        <Counter {...counterProps} />
+      </div>
+    </Container>
   );
 }
 
--- src/Counter.js
+++ src/Counter.js
@@ -1,32 +1,16 @@
 import React, { useState } from 'react';
+import OddEvenResult from './OddEvenResult';
 
-const Counter = () => {
-  // 0에서 출발
-  // 1씩 증가하고
-  // 1씩 감소하는
-  // count 상태
+const Counter = ({ initialValue }) => {
+  // 객체 비구조화 할당
+  const [count, setCount] = useState(initialValue);
 
-  // 컴포넌트는 자신의 state가 변경되면 rerender!
-  console.log('counter 호출');
-
-  // 배열 비구조화 할당
-  // count: 상태
-  // setCount: 상태 변화 함수
-  // 0: 초기값
-  const [count, setCount] = useState(0);
   const onIncrese = () => {
     setCount(count + 1);
   };
+
   const onDecrese = () => {
     setCount(count - 1);
-  };
-  // 한 컴포넌트가 여러 개의 state를 가질 수도 있음
-  const [count2, setCount2] = useState(0);
-  const onIncrese2 = () => {
-    setCount2(count2 + 1);
-  };
-  const onDecrese2 = () => {
-    setCount2(count2 - 1);
   };
 
   return (
@@ -34,11 +18,14 @@ const Counter = () => {
       <h2>{count}</h2>
       <button onClick={onIncrese}>+</button>
       <button onClick={onDecrese}>-</button>
-      <h2>{count2}</h2>
-      <button onClick={onIncrese2}>+</button>
-      <button onClick={onDecrese2}>-</button>
+      <OddEvenResult count={count} />
     </div>
   );
+};
+
+// 전달받지 못한 prop의 기본 값 설정
+Counter.defaultProps = {
+  initialValue: 0,
 };
 
 export default Counter;
